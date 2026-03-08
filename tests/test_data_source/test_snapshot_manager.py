332bf7e384b8d22bf00c6dcfd9fb039a784b0faa
Aditya Rajpurohit <adi.at.internet@gmail.com>
2026-03-07 21:53:51 -0800

    fix test failure

--- tests/test_data_source/test_snapshot_manager.py
+++ tests/test_data_source/test_snapshot_manager.py
@@ -6,11 +6,11 @@ from app.data_source.snapshot_manager import SnapshotManager
 @pytest.mark.asyncio
 async def test_get_snapshot():
 
-    sm = SnapshotManager()
+    snapshot_manager = SnapshotManager()
 
     # manually inject cache
-    sm._cache["x"] = {"tables": []}
+    snapshot_manager._cache["x"] = {"tables": []}
 
-    snapshot = await sm.get_snapshot("x")
+    snapshot = await snapshot_manager.get_snapshot("x")
 
     assert snapshot == {"tables": []}
